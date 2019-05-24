589eac8d8ccb19893c6dc66d8f0de769537b3016
Rob McDougall <rmcdougawww@rogers.com>
2019-05-24 10:42:39 -0400

    Finished Adapter.  Unit tests only test happy path.  Need to add some more unhappy path and SSL tests.

--- rest-services/client/src/main/java/com/_4point/aem/docservices/rest_services/client/forms/RestServicesFormsServiceAdapter.java
+++ rest-services/client/src/main/java/com/_4point/aem/docservices/rest_services/client/forms/RestServicesFormsServiceAdapter.java
@@ -1,6 +1,7 @@
 package com._4point.aem.docservices.rest_services.client.forms;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.function.Supplier;
 
 import javax.ws.rs.client.Client;
@@ -9,6 +10,8 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status.Family;
+import javax.ws.rs.core.Response.StatusType;
 
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 
@@ -16,6 +19,7 @@ import com._4point.aem.fluentforms.api.Document;
 import com._4point.aem.fluentforms.api.forms.FormsService.FormsServiceException;
 import com._4point.aem.fluentforms.api.forms.ValidationOptions;
 import com._4point.aem.fluentforms.api.forms.ValidationResult;
+import com._4point.aem.fluentforms.impl.SimpleDocumentFactoryImpl;
 import com._4point.aem.fluentforms.impl.forms.TraditionalFormsService;
 import com.adobe.fd.forms.api.DataFormat;
 import com.adobe.fd.forms.api.PDFFormRenderOptions;
@@ -64,11 +68,25 @@ public class RestServicesFormsServiceAdapter implements TraditionalFormsService 
 								    .post(Entity.entity(multipart, multipart.getMediaType()));
 			
 			// TODO: Check the result to make sure everything is OK.
-			// TODO: Turn the result into a Document object
+			StatusType resultStatus = result.getStatusInfo();
+			if (!Family.SUCCESSFUL.equals(resultStatus.getFamily())) {
+				String message = "Call to server failed, statusCode='" + resultStatus.getStatusCode() + "', reason='" + resultStatus.getReasonPhrase() + "'.";
+				if (result.hasEntity()) {
+					InputStream entityStream = (InputStream) result.getEntity();
+					//  TODO: Transfer the entity into the message.
+				}
+				throw new FormsServiceException(message);
+			}
+			
+			if (!result.hasEntity()) {
+				throw new FormsServiceException("Call to server succeeded but server failed to return document.  This should never happen.");
+			}
+			
+			return SimpleDocumentFactoryImpl.getFactory().create((InputStream) result.getEntity());
+			
 		} catch (IOException e) {
 			throw new FormsServiceException("I/O Error while importing data. (" + target.getUri().toString() + ").", e);
 		}
-		return null;
 	}
 
 	@Override
--- rest-services/client/src/test/java/com/_4point/aem/docservices/rest_services/client/forms/RestServicesFormsServiceAdapterTest.java
+++ rest-services/client/src/test/java/com/_4point/aem/docservices/rest_services/client/forms/RestServicesFormsServiceAdapterTest.java
@@ -5,11 +5,16 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.StatusType;
 
+import org.apache.commons.io.IOUtils;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -22,8 +27,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com._4point.aem.fluentforms.api.Document;
 import com._4point.aem.fluentforms.api.forms.FormsService.FormsServiceException;
+import com._4point.aem.fluentforms.testing.MockDocumentFactory;
 
 import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class RestServicesFormsServiceAdapterTest {
@@ -37,10 +48,12 @@ class RestServicesFormsServiceAdapterTest {
 	@Mock WebTarget target;
 	@Mock Response response;
 	@Mock Builder builder;
+	@Mock StatusType statusType;
 	
 	@Captor ArgumentCaptor<String> machineName;
 	@Captor ArgumentCaptor<String> path;
-	
+	@Captor ArgumentCaptor<Entity> entity;
+
 	RestServicesFormsServiceAdapter underTest;
 	
 	@BeforeEach
@@ -55,16 +68,22 @@ class RestServicesFormsServiceAdapterTest {
 	@Test
 	void testImportData_noSsl() throws Exception {
 
+		Document responseData = MockDocumentFactory.GLOBAL_INSTANCE.create("response Document Data".getBytes());
+		
 		when(client.target(machineName.capture())).thenReturn(target);
 		when(target.path(path.capture())).thenReturn(target);
 		when(target.request()).thenReturn(builder);
 		when(builder.accept(APPLICATION_PDF)).thenReturn(builder);
-		when(builder.post(any())).thenReturn(response);
+		when(builder.post(entity.capture())).thenReturn(response);
+		when(response.getStatusInfo()).thenReturn(statusType);
+		when(statusType.getFamily()).thenReturn(Response.Status.Family.SUCCESSFUL);	// return Successful
+		when(response.hasEntity()).thenReturn(true);
+		when(response.getEntity()).thenReturn(new ByteArrayInputStream(responseData.getInlineData()));
 		
 		underTest = new RestServicesFormsServiceAdapter(TEST_MACHINE_NAME, TEST_MACHINE_PORT, false, ()->client);
 		
-		Document pdf = Mockito.mock(Document.class);
-		Document data = Mockito.mock(Document.class);
+		Document pdf = MockDocumentFactory.GLOBAL_INSTANCE.create("pdf Document Data".getBytes());
+		Document data = MockDocumentFactory.GLOBAL_INSTANCE.create("data Document Data".getBytes());
 
 		Document pdfResult = underTest.importData(pdf, data);
 		
@@ -74,11 +93,48 @@ class RestServicesFormsServiceAdapterTest {
 		assertThat("Expected target url contains TEST_MACHINE_PORT", machineName.getValue(), containsString(Integer.toString(TEST_MACHINE_PORT)));
 		assertThat("Expected target url contains 'ImportData'", path.getValue(), containsString("ImportData"));
 
-		// TODO: Make sure that the arguments we passed in are transmitted correctly.
+		// Make sure that the arguments we passed in are transmitted correctly.
+		@SuppressWarnings("unchecked")
+		Entity<FormDataMultiPart> postedEntity = (Entity<FormDataMultiPart>)entity.getValue();
+		FormDataMultiPart postedData = postedEntity.getEntity();
 		
-		// TODO: Make sure the response is handled correctly.
+		assertEquals(MediaType.MULTIPART_FORM_DATA_TYPE, postedEntity.getMediaType());
+		validateDocumentFormField(postedData, "pdf", new MediaType("application", "pdf"), pdf.getInlineData());
+		validateDocumentFormField(postedData, "data", MediaType.APPLICATION_XML_TYPE, data.getInlineData());
+		
+		// Make sure the response is correct.
+		assertArrayEquals(responseData.getInlineData(), pdfResult.getInlineData());
 	}
 	
+	private void validateDocumentFormField(FormDataMultiPart postedData, String fieldName, MediaType expectedMediaType, byte[] expectedData) throws IOException {
+		List<FormDataBodyPart> pdfFields = postedData.getFields(fieldName);
+		assertEquals(1, pdfFields.size());
+		
+		FormDataBodyPart pdfPart = pdfFields.get(0);
+		assertEquals(expectedMediaType, pdfPart.getMediaType());
+		byte[] pdfBytes = IOUtils.toByteArray((InputStream) pdfPart.getEntity());
+		assertArrayEquals(expectedData, pdfBytes);  // TODO: Need to figure out how to test for entity.
+	}
+
+	@Test
+	void testImportData_SuccessButNoEntity() throws Exception {
+
+		when(client.target(machineName.capture())).thenReturn(target);
+		when(target.path(path.capture())).thenReturn(target);
+		when(target.request()).thenReturn(builder);
+		when(builder.accept(APPLICATION_PDF)).thenReturn(builder);
+		when(builder.post(entity.capture())).thenReturn(response);
+		when(response.getStatusInfo()).thenReturn(statusType);
+		when(statusType.getFamily()).thenReturn(Response.Status.Family.SUCCESSFUL);	// return Successful
+		
+		underTest = new RestServicesFormsServiceAdapter(TEST_MACHINE_NAME, TEST_MACHINE_PORT, false, ()->client);
+		
+		Document pdf = MockDocumentFactory.GLOBAL_INSTANCE.create("pdf Document Data".getBytes());
+		Document data = MockDocumentFactory.GLOBAL_INSTANCE.create("data Document Data".getBytes());
+
+		FormsServiceException ex = assertThrows(FormsServiceException.class, ()->underTest.importData(pdf, data));
+		assertThat(ex.getMessage(), containsString("should never happen"));
+	}
 	@Disabled
 	void testRenderPDFForm() {
 		fail("Not yet implemented");
