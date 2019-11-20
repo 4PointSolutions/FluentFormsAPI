e89c91a8b7be35c7172a9b750343f088e7e35b42
Rob McDougall <rmcdougawww@rogers.com>
2019-11-20 17:19:16 -0500

    Added some integration tests for GeneratePDFOutput.

--- rest-services/it.tests/src/test/java/com/_4point/aem/docservices/rest_services/it_tests/client/output/GeneratePdfOutputTest.java
+++ rest-services/it.tests/src/test/java/com/_4point/aem/docservices/rest_services/it_tests/client/output/GeneratePdfOutputTest.java
@@ -1,99 +1,92 @@
-package com._4point.aem.docservices.rest_services.it_tests.client.forms;
+package com._4point.aem.docservices.rest_services.it_tests.client.output;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Locale;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com._4point.aem.docservices.rest_services.client.output.RestServicesOutputServiceAdapter;
+import com._4point.aem.docservices.rest_services.it_tests.ByteArrayString;
+import com._4point.aem.fluentforms.api.Document;
+import com._4point.aem.fluentforms.api.PathOrUrl;
+import com._4point.aem.fluentforms.api.output.OutputService;
+import com._4point.aem.fluentforms.impl.SimpleDocumentFactoryImpl;
+import com._4point.aem.fluentforms.impl.UsageContext;
+import com._4point.aem.fluentforms.impl.output.OutputServiceImpl;
+import com.adobe.fd.output.api.AcrobatVersion;
 
 import static com._4point.aem.docservices.rest_services.it_tests.TestUtils.*;
 
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.*;
-
-import com._4point.aem.docservices.rest_services.client.forms.RestServicesFormsServiceAdapter;
-import com._4point.aem.docservices.rest_services.it_tests.ByteArrayString;
-import com._4point.aem.docservices.rest_services.it_tests.TestUtils;
-import com._4point.aem.fluentforms.api.Document;
-import com._4point.aem.fluentforms.api.PathOrUrl;
-import com._4point.aem.fluentforms.api.forms.FormsService;
-import com._4point.aem.fluentforms.impl.SimpleDocumentFactoryImpl;
-import com._4point.aem.fluentforms.impl.UsageContext;
-import com._4point.aem.fluentforms.impl.forms.FormsServiceImpl;
-import com.adobe.fd.forms.api.AcrobatVersion;
-import com.adobe.fd.forms.api.CacheStrategy;
-
-class RenderPdfFormTest {
+class GeneratePdfOutputTest {
 
 	private static final String CRX_CONTENT_ROOT = "crx:/content/dam/formsanddocuments/sample-forms";
 
-	private FormsService underTest; 
-
+	private OutputService underTest;
+	
 	@BeforeEach
 	void setUp() throws Exception {
-		RestServicesFormsServiceAdapter adapter = RestServicesFormsServiceAdapter.builder()
+		RestServicesOutputServiceAdapter adapter = RestServicesOutputServiceAdapter.builder()
 				.machineName(TEST_MACHINE_NAME)
 				.port(TEST_MACHINE_PORT)
 				.basicAuthentication(TEST_USER, TEST_USER_PASSWORD)
 				.useSsl(false)
 				.build();
 
-		underTest = new FormsServiceImpl(adapter, UsageContext.SERVER_SIDE);
+		underTest = new OutputServiceImpl(adapter, UsageContext.SERVER_SIDE);
 	}
 
 	@Test
-	@DisplayName("Test renderPdfForm() Just Form and Data.")
-	void testRenderPdfForm_JustFormAndData() throws Exception {
-		Document pdfResult =  underTest.renderPDFForm()
+	@DisplayName("Test generatePdfOutput() Just Form and Data.")
+	void testGeneratePdfOutput_JustFormAndData() throws Exception {
+		Document pdfResult =  underTest.generatePDFOutput()
 									.executeOn(SAMPLE_FORM_XDP, SimpleDocumentFactoryImpl.getFactory().create(SAMPLE_FORM_DATA_XML.toFile()));
 		
 		assertThat("Expected a PDF to be returned.", ByteArrayString.toString(pdfResult.getInlineData(), 8), containsString("%, P, D, F, -, 1, ., 7"));
-		IOUtils.write(pdfResult.getInlineData(), Files.newOutputStream(ACTUAL_RESULTS_DIR.resolve("RenderPdfFormClient_JustFormAndData.pdf")));
+		IOUtils.write(pdfResult.getInlineData(), Files.newOutputStream(ACTUAL_RESULTS_DIR.resolve("GeneratePdfOutput_JustFormAndData.pdf")));
 		
 	}
 
 	@Test
-	@DisplayName("Test renderPdfForm() CRX Form and Data.")
-	void testRenderPdfForm_CRXFormAndData() throws Exception {
-		Document pdfResult =  underTest.renderPDFForm()
+	@DisplayName("Test generatePdfOutput() CRX Form and Data.")
+	void testGeneratePdfOutput_CRXFormAndData() throws Exception {
+		Document pdfResult =  underTest.generatePDFOutput()
 									.setContentRoot(PathOrUrl.fromString(CRX_CONTENT_ROOT))
 									.executeOn(SAMPLE_FORM_XDP.getFileName(), SimpleDocumentFactoryImpl.getFactory().create(SAMPLE_FORM_DATA_XML.toFile()));
 		
 		assertThat("Expected a PDF to be returned.", ByteArrayString.toString(pdfResult.getInlineData(), 8), containsString("%, P, D, F, -, 1, ., 7"));
-		IOUtils.write(pdfResult.getInlineData(), Files.newOutputStream(ACTUAL_RESULTS_DIR.resolve("RenderPdfFormClient_CRXFormAndData.pdf")));
+		IOUtils.write(pdfResult.getInlineData(), Files.newOutputStream(ACTUAL_RESULTS_DIR.resolve("GeneratePdfOutput_CRXFormAndData.pdf")));
 		
 	}
 
 	@Test
-	@DisplayName("Test renderPdfForm() All Arguments.")
-	void testRenderPdfForm_AllArgs() throws Exception {
+	@DisplayName("Test generatePdfOutput() All Arguments.")
+	void testGeneratePdfOutput_AllArgs() throws Exception {
 		AcrobatVersion acrobatVersion = AcrobatVersion.Acrobat_10_1;
-		CacheStrategy strategy = CacheStrategy.NONE;
 		Path contentRoot = SAMPLE_FORM_XDP.getParent();
 //		Path debugDir = null;
-		Locale locale = Locale.CANADA_FRENCH;
-//		String submitUrl = "";
-		Document xci = SimpleDocumentFactoryImpl.INSTANCE.create(RESOURCES_DIR.resolve("pa.xci"));
 		
-		Document pdfResult =  underTest.renderPDFForm()
+		Document pdfResult =  underTest.generatePDFOutput()
 									.setAcrobatVersion(acrobatVersion)
-									.setCacheStrategy(strategy)
 									.setContentRoot(contentRoot)
 //									.setDebugDir(debugDir)
-									.setLocale(locale)
-//									.setSubmitUrlString(submitUrl)
+									.setEmbedFonts(true)
+									.setLinearizedPDF(true)
+									.setLocale(Locale.CANADA_FRENCH)
+									.setRetainPDFFormState(true)
+									.setRetainUnsignedSignatureFields(true)
 									.setTaggedPDF(true)
-									.setXci(xci)
 									.executeOn(SAMPLE_FORM_XDP, SimpleDocumentFactoryImpl.getFactory().create(SAMPLE_FORM_DATA_XML.toFile()));
 		
 		assertThat("Expected a PDF to be returned.", ByteArrayString.toString(pdfResult.getInlineData(), 8), containsString("%, P, D, F, -, 1, ., 7"));
-		IOUtils.write(pdfResult.getInlineData(), Files.newOutputStream(ACTUAL_RESULTS_DIR.resolve("RenderPdfFormClient_AllArgs.pdf")));
+		IOUtils.write(pdfResult.getInlineData(), Files.newOutputStream(ACTUAL_RESULTS_DIR.resolve("GeneratePdfOutput_AllArgs.pdf")));
 		
 	}
-
-
 }
