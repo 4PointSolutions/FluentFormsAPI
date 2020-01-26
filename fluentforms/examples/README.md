ac247ef6d6a7c02b8ca5ddf8208e797f94af849f
Rob McDougall <rmcdougawww@rogers.com>
2020-01-26 15:36:56 -0500

    Added ann examples project.  This will contain examples of how to invoke the fluent forms apis.

---
+++ fluentforms/examples/README.md
@@ -0,0 +1,12 @@
+# AEM Forms Fluent API Examples
+
+This sub-project provides examples of using the Fluent Forms API for AEM Forms-based applications.
+
+## Goals of this sub-project
+
+This maven sub-project provides examples of how to call the fluentforms API.
+
+In the short term, it provides samples that compile (and should work if run).
+
+In the longer term, we expect to create a bundle that can be deployed and executed on the server (in order to ensure that the samples are actually correct and produce the expected results on the server).
+  
---
+++ fluentforms/examples/src/test/java/com/_4point/aem/fluentforms/examples/OutputServiceExamples.java
@@ -0,0 +1,102 @@
+package com._4point.aem.fluentforms.examples;
+
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Locale;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.osgi.service.component.annotations.Reference;
+
+import com._4point.aem.fluentforms.api.Document;
+import com._4point.aem.fluentforms.api.DocumentFactory;
+import com._4point.aem.fluentforms.api.PathOrUrl;
+import com._4point.aem.fluentforms.api.output.OutputService;
+import com._4point.aem.fluentforms.factory.ServerFactory;
+import com.adobe.fd.output.api.AcrobatVersion;
+
+class OutputServiceExamples {
+
+	@Reference
+	private com.adobe.fd.output.api.OutputService adobeOutputService;
+
+	private String sampleData = "<foo><bar>dfljlsdfsf</bar></foo>";
+	private String sampleXci = "<foo><bar>dfljlsdfsf</bar></foo>";
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void demoGeneratePdfOutputMinimal() throws Exception {
+		OutputService outputService = ServerFactory.createOutputService(adobeOutputService);
+		
+		// 
+		Document data = ServerFactory.getDefaultDocumentFactory().create(sampleData.getBytes());
+		
+		Document result = outputService.generatePDFOutput()
+								.executeOn(Paths.get("foo/bar.xdp"), data );
+		
+		byte[] pdfBytes = result.getInlineData();	// you can do either of these but not both.
+//		InputStream pdfStream = result.getInputStream();
+	}
+
+	@Test
+	void demoGeneratePdfOutputMaximal() throws Exception {
+		OutputService outputService = ServerFactory.createOutputService(adobeOutputService);
+		
+		// 
+		DocumentFactory docFactory = ServerFactory.getDefaultDocumentFactory();
+		Document data = docFactory.create(sampleData.getBytes());
+		
+		Document result = outputService.generatePDFOutput()
+								.setContentRoot(Paths.get("Adobe", "forms"))
+								.setAcrobatVersion(AcrobatVersion.Acrobat_11)
+								.setDebugDir(Paths.get("Adobe", "debugDir"))
+								.setEmbedFonts(true)
+								.setLinearizedPDF(true)
+								.setLocale(Locale.CANADA_FRENCH)
+								.setRetainPDFFormState(false)
+								.setRetainUnsignedSignatureFields(true)
+								.setTaggedPDF(true)
+								.setXci(docFactory.create(sampleXci.getBytes()))
+								.executeOn(Paths.get("foo", "bar.xdp"), data );
+		
+//		byte[] pdfBytes = result.getInlineData();	// you can do either of these but not both.
+		InputStream pdfStream = result.getInputStream();
+	}
+
+	@Test
+	void demoGeneratePdfOutputTypical() throws Exception {
+		OutputService outputService = ServerFactory.createOutputService(adobeOutputService);
+		
+		// 
+		Document data = ServerFactory.getDefaultDocumentFactory().create(sampleData.getBytes());
+		
+		Document result = outputService.generatePDFOutput()
+								.setContentRoot(Paths.get("Adobe", "forms"))
+								.setTaggedPDF(true)
+								.executeOn(Paths.get("foo/bar.xdp"), data );
+		
+		byte[] pdfBytes = result.getInlineData();	// you can do either of these but not both.
+//		InputStream pdfStream = result.getInputStream();
+	}
+
+	@Test
+	void demoGeneratePdfOutputTypical2() throws Exception {
+		OutputService outputService = ServerFactory.createOutputService(adobeOutputService);
+		
+		// 
+		Document data = ServerFactory.getDefaultDocumentFactory().create(sampleData.getBytes());
+		
+		Document result = outputService.generatePDFOutput()
+								.setContentRoot(PathOrUrl.fromString("crx:/content/dam/formsanddocuments/"))
+								.setTaggedPDF(true)
+								.executeOn(Paths.get("foo/bar.xdp"), data );
+		
+//		byte[] pdfBytes = result.getInlineData();	// you can do either of these but not both.
+		InputStream pdfStream = result.getInputStream();
+	}
+
+
+}
