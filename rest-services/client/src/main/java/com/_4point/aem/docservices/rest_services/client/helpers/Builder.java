88f20d9b1e7752c871ba3c363a7af28c3fbd338c
Rob McDougall <rmcdougawww@rogers.com>
2020-06-01 10:42:48 -0400

    I ended up re-using some code from formsfeeder.server and rest-services.client in the formsfeed.client project.  The files are small, and therefore not worth creating a whole new project so that the code can be shared, so the files are copied instead.  I've included comments in the file to indicate that they are shared, so that future changes can be propagated across into the other projects.

--- rest-services/client/src/main/java/com/_4point/aem/docservices/rest_services/client/helpers/Builder.java
+++ rest-services/client/src/main/java/com/_4point/aem/docservices/rest_services/client/helpers/Builder.java
@@ -4,6 +4,15 @@ import java.util.function.Supplier;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 
+/**
+ * This interface is is for a Builder object that assists in building a JAX-RS client.  It is shared between the following projects:
+ * fluentforms/rest-services.client
+ * formsfeeder.client
+ * 
+ * If changes are made to this file, those changes should be propagated into each of the other projects.
+ * These files are copied because there is not enough code to justify creating another project and adding another dependency. 
+ *
+ */
 public interface Builder {
 
 	public Builder machineName(String machineName);
--- rest-services/client/src/main/java/com/_4point/aem/docservices/rest_services/client/helpers/BuilderImpl.java
+++ rest-services/client/src/main/java/com/_4point/aem/docservices/rest_services/client/helpers/BuilderImpl.java
@@ -9,6 +9,15 @@ import javax.ws.rs.client.WebTarget;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
+/**
+ * This class provides an implementation of the Builder interface that assists in building a JAX-RS client.  It is shared between the following projects:
+ * fluentforms/rest-services.client
+ * formsfeeder.client
+ * 
+ * If changes are made to this file, those changes should be propagated into each of the other projects. 
+ * These files are copied because there is not enough code to justify creating another project and adding another dependency. 
+ *
+ */
 public class BuilderImpl implements Builder {
 	private final static Supplier<Client> defaultClientFactory = ()->ClientBuilder.newClient();
 	
