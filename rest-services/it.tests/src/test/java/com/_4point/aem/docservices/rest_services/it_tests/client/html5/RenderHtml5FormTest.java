4077d52543180e90a499c0c54c1b0be5e88d191b
Rob McDougall <rmcdougawww@rogers.com>
2020-07-09 16:38:40 -0400

    Completed client integration tests for Html5 forms.

--- rest-services/it.tests/src/test/java/com/_4point/aem/docservices/rest_services/it_tests/client/html5/RenderHtml5FormTest.java
+++ rest-services/it.tests/src/test/java/com/_4point/aem/docservices/rest_services/it_tests/client/html5/RenderHtml5FormTest.java
@@ -1,21 +1,25 @@
 package com._4point.aem.docservices.rest_services.it_tests.client.html5;
 
-import static com._4point.aem.docservices.rest_services.it_tests.TestUtils.SAMPLE_FORM_DATA_XML;
-import static com._4point.aem.docservices.rest_services.it_tests.TestUtils.SAMPLE_FORM_PDF;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com._4point.aem.docservices.rest_services.client.html5.Html5FormsService;
-import com._4point.aem.docservices.rest_services.client.html5.Html5FormsService.Html5FormsServiceException;
+import com._4point.aem.docservices.rest_services.it_tests.HtmlFormDocument;
 import com._4point.aem.docservices.rest_services.it_tests.TestUtils;
 import com._4point.aem.fluentforms.api.Document;
 import com._4point.aem.fluentforms.api.PathOrUrl;
 import com._4point.aem.fluentforms.impl.SimpleDocumentFactoryImpl;
 
 class RenderHtml5FormTest {
-	private static final String CRX_CONTENT_ROOT = "crx:/content/dam/formsanddocuments/sample-forms";
+	private static final String CRX_CONTENT_ROOT = "crx:/content/dam/formsanddocuments/sample-forms/";
 	
 	
 	private Html5FormsService underTest; 
@@ -38,17 +42,35 @@ class RenderHtml5FormTest {
 		
 		Document result = underTest.renderHtml5Form(template);
 		
-		// Do something to verify the result;
+		// Verify the result;
+		byte[] resultBytes = IOUtils.toByteArray(result.getInputStream());
+		HtmlFormDocument htmlDoc = HtmlFormDocument.create(resultBytes, new URI("http://" + TestUtils.TEST_MACHINE_NAME + ":" + TestUtils.TEST_MACHINE_PORT));
+		assertEquals("LC Forms", htmlDoc.getTitle());
+
+		// Make sure the data wasn't populated.
+		String html = new String(resultBytes, StandardCharsets.UTF_8);
+		// Does not contain field data, does not contain auth string but *does* contain environment name.
+		assertThat(html, not(anyOf(containsString("Text Field1 Data"), containsString("Text Field2 Data"))));
+	
 	}
 
 	@Test
 	void testRenderHtml5FormPathOrUrlDocument() throws Exception {
-		PathOrUrl template = PathOrUrl.from(TestUtils.SAMPLE_FORM_XDP.getFileName());
-		Document data = SimpleDocumentFactoryImpl.INSTANCE.create(SAMPLE_FORM_DATA_XML.toFile());
+		PathOrUrl template = PathOrUrl.from(CRX_CONTENT_ROOT + TestUtils.SAMPLE_FORM_XDP.getFileName().toString());
+		Document data = SimpleDocumentFactoryImpl.INSTANCE.create(TestUtils.SAMPLE_FORM_DATA_XML.toFile());
+		data.setContentTypeIfEmpty("application/xml");
 		
 		Document result = underTest.renderHtml5Form(template, data);
-		
-		// Do something to verify the result;
+
+		// Verify the result;
+		byte[] resultBytes = IOUtils.toByteArray(result.getInputStream());
+		HtmlFormDocument htmlDoc = HtmlFormDocument.create(resultBytes, new URI("http://" + TestUtils.TEST_MACHINE_NAME + ":" + TestUtils.TEST_MACHINE_PORT));
+		assertEquals("LC Forms", htmlDoc.getTitle());
+
+		// Make sure the data was populated.
+		String html = new String(resultBytes, StandardCharsets.UTF_8);
+		// Contains field data, does not contain auth string but *does* contain environment name.
+		assertThat(html, allOf(containsString("Text Field1 Data"), containsString("Text Field2 Data")));
 	}
 
 }
