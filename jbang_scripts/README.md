f504746f8dd108137d76a06eac4f062d1b863505
Rob McDougall <rmcdougawww@rogers.com>
2022-11-15 13:01:26 -0500

    Added a JBang script for calling the OutputService.

---
+++ jbang_scripts/README.md
@@ -0,0 +1,6 @@
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+# JBang scripts
+This directory contains [JBang](https://jbang.dev/) scripts that invoke AEM via the FluentForms REST Services APIs.
+
+While being useful in and of themselves, they also provide example code for how to invoke the FluentForms REST Services.
---
+++ jbang_scripts/invoke_output.java
@@ -0,0 +1,95 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//REPOS mavencentral,github=https://maven.pkg.github.com/4PointSolutions/*
+//DEPS info.picocli:picocli:4.6.3
+//DEPS com._4point.aem:fluentforms.core:0.0.2-SNAPSHOT  com._4point.aem.docservices:rest-services.client:0.0.2-SNAPSHOT
+//JAVA 11+
+
+/*
+ * 	This script uses the 4PointSolutions/FormsFeeder GitHub package repository.  GitHub requires a user to authenticate in order
+ * 	to access a package repository.  In order for this script to work, you need to have your personal credentials configured
+ * 	in your local settings.xml file (found in you $HOME/.m2 directory).
+ * 	
+ *  Your settings.xml should look something like this:
+ *  	<?xml version="1.0"?>
+ *  	<settings xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://maven.apache.org/SETTINGS/1.0.0">
+ *  		<servers>
+ *  			<server>
+ *  				<id>github</id>
+ *  				<username>Your GitHub Username goes here</username>
+ *  				<password>Your Personal Access Token goes here</password>
+ *  			</server>
+ *  		</servers>
+ *  	</settings>
+ * 
+ */
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.Callable;
+
+import com._4point.aem.docservices.rest_services.client.output.RestServicesOutputServiceAdapter;
+import com._4point.aem.fluentforms.api.Document;
+import com._4point.aem.fluentforms.impl.SimpleDocumentFactoryImpl;
+import com._4point.aem.fluentforms.impl.UsageContext;
+import com._4point.aem.fluentforms.impl.output.OutputServiceImpl;
+import com.adobe.fd.output.api.AcrobatVersion;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "invoke_aem", mixinStandardHelpOptions = true, version = "invoke_aem 0.1",
+        description = "invoke_aem made with jbang")
+class invoke_aem implements Callable<Integer> {
+
+    @Option(names = {"-f", "--form"}, description = "A filepath to the XDP.", required = true)
+    private Path xdpLocation;
+
+    @Option(names = {"-d", "--data"}, description = "A filepath to the XML data file. (Optional)")
+    private Path xmlLocation;
+
+    @Option(names = {"-o", "--output"}, description = "A filepath to the output file.", required = true)
+    private Path outLocation;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new invoke_aem()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+    	
+		var adapter = RestServicesOutputServiceAdapter.builder()
+				.machineName("localhost")
+				.port(4502)
+				.basicAuthentication("admin", "admin")
+				.useSsl(false)
+				.build();
+
+		var underTest = new OutputServiceImpl(adapter, UsageContext.CLIENT_SIDE);
+		
+		
+		var builder = underTest.generatePDFOutput()
+				.setAcrobatVersion(AcrobatVersion.Acrobat_10_1)
+				.setEmbedFonts(true)
+				// Less used parameters are commented out which leaves them at default.
+//				.setLinearizedPDF(true)
+//				.setLocale(Locale.CANADA_FRENCH)
+//				.setRetainPDFFormState(true)
+//				.setRetainUnsignedSignatureFields(true)
+				.setTaggedPDF(true);
+
+		if (Files.exists(xdpLocation)) {
+			builder = builder.setContentRoot(xdpLocation.toAbsolutePath().getParent());
+		}
+		
+		Document pdfResult =  xmlLocation == null ? 
+				builder.executeOn(xdpLocation.getFileName(), null) :
+				builder.executeOn(xdpLocation.getFileName(), SimpleDocumentFactoryImpl.getFactory().create(xmlLocation.toFile()));
+
+		System.out.println("Writing output to " + outLocation);
+		Files.copy(pdfResult.getInputStream(), outLocation, StandardCopyOption.REPLACE_EXISTING);
+		
+        return 0;
+    }
+}
