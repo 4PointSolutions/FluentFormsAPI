a697507430bf0b763d6305faf95cb3a8492a535e
Rob McDougall <rmcdougawww@rogers.com>
2023-04-24 09:38:52 -0400

    Added LogLevel back into the impl package, but made it deprecated.
    The move of this class was causing backwards compatibility issues.

---
+++ fluentforms/core/src/main/java/com/_4point/aem/fluentforms/impl/assembler/LogLevel.java
@@ -0,0 +1,11 @@
+package com._4point.aem.fluentforms.impl.assembler;
+
+/**
+ * Use com._4point.aem.fluentforms.api.assembler.LogLevel instead.
+ *
+ */
+@Deprecated
+public enum LogLevel {
+	
+	OFF, SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST, ALL 
+}
