0c7551288d3741e6db4766d7d08b65d6faf54177
Rob McDougall <rmcdougawww@rogers.com>
2023-05-24 14:12:17 -0400

    Created Spring Boot sample application project skeleton.

---
+++ spring/fluentforms-spring-boot-sample-app/src/main/java/com/_4point/aem/fluentforms/sampleapp/FluentFormsSpringApplication.java
@@ -0,0 +1,13 @@
+package com._4point.aem.fluentforms.sampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluentFormsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluentFormsSpringApplication.class, args);
+	}
+
+}
---
+++ spring/fluentforms-spring-boot-sample-app/src/test/java/com/_4point/aem/fluentforms/sampleapp/FluentFormsSpringApplicationTest.java
@@ -0,0 +1,15 @@
+package com._4point.aem.fluentforms.sampleapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluentFormsSpringApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
