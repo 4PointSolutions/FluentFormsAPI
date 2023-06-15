2b0154d44e83913dabcc1bf9a608acaa76a41cc1
Rob McDougall <rmcdougawww@rogers.com>
2023-06-15 16:10:25 -0400

    Set up DataService to fall back to InMemory if no folder is provided.

--- spring/fluentforms-sample-web-app/src/main/java/com/_4point/aem/fluentforms/sampleapp/FluentFormsSpringApplication.java
+++ spring/fluentforms-sample-web-app/src/main/java/com/_4point/aem/fluentforms/sampleapp/FluentFormsSpringApplication.java
@@ -1,14 +1,21 @@
 package com._4point.aem.fluentforms.sampleapp;
 
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com._4point.aem.fluentforms.sampleapp.domain.DataService;
 import com._4point.aem.fluentforms.sampleapp.ports.InMemoryDataService;
+import com._4point.aem.fluentforms.sampleapp.ports.LocalFolderDataService;
 
 @SpringBootApplication
 public class FluentFormsSpringApplication {
+	private final static Logger log = LoggerFactory.getLogger(FluentFormsSpringApplication.class);
 
 	public FluentFormsSpringApplication(GitConfig gitConfig) {
     	gitConfig.logGitInformation();
@@ -18,13 +25,14 @@ public class FluentFormsSpringApplication {
 		SpringApplication.run(FluentFormsSpringApplication.class, args);
 	}
 
-//	@Bean
-//	public static DataService dataService(@Value("${sampleapp.dataservice.folder}") Path dataFolder) {
-//		return new LocalFolderDataService(dataFolder);
-//	}
-	
 	@Bean
-	public static DataService dataService() {
-		return new InMemoryDataService();
+	public static DataService dataService(@Value("${mockclient.dataservice.folder:}") Path dataFolder) {
+		if(dataFolder == null || dataFolder.toString().isEmpty()) {
+			log.atInfo().log("Using InMemory Data Service.");
+			return new InMemoryDataService();					// If folder not provided, use InMemory service
+		} else {
+			log.atInfo().addArgument(dataFolder).log("Using Local Folder Data Service to folder {}.");
+			return new LocalFolderDataService(dataFolder);
+		}
 	}
 }
