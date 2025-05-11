f0f3ac985c46b9583a46d699c6861aa98160ce2a
Rob McDougall <rmcdougawww@rogers.com>
2025-05-11 09:56:31 -0400

    🧪 Added test for testing proxy code.
    This is in preparation for fixing #52

--- spring/fluentforms-sample-web-app/src/test/java/com/_4point/aem/fluentforms/sampleapp/resources/AemProxyEndpointTest.java
+++ spring/fluentforms-sample-web-app/src/test/java/com/_4point/aem/fluentforms/sampleapp/resources/AemProxyEndpointTest.java
@@ -2,12 +2,14 @@ package com._4point.aem.fluentforms.sampleapp.resources;
 
 import static com._4point.testing.matchers.jaxrs.ResponseMatchers.hasMediaType;
 import static com._4point.testing.matchers.jaxrs.ResponseMatchers.isStatus;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com._4point.testing.matchers.jaxrs.ResponseMatchers.hasEntity;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.net.URI;
+import java.nio.file.Path;
 import java.util.List;
 
 import jakarta.ws.rs.client.ClientBuilder;
@@ -15,13 +17,18 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 
+import org.htmlunit.DefaultCredentialsProvider;
+import org.htmlunit.WebClient;
+import org.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import com._4point.aem.docservices.rest_services.client.RestClient;
 import com._4point.aem.fluentforms.sampleapp.FluentFormsSpringApplication;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.http.ResponseDefinition;
@@ -39,6 +46,9 @@ class AemProxyEndpointTest {
 	private static final boolean SAVE_RESULTS = false;
 	private static final boolean WIREMOCK_RECORDING = false;
 	static final int WIREMOCK_PORT = 5502;
+
+	private static final Path RESOURCES_DIR = Path.of("src", "test", "resources");
+	private static final Path SAMPLE_FILES_DIR = RESOURCES_DIR.resolve("SampleFiles");
 
 	@LocalServerPort
 	private int port;
@@ -91,6 +101,67 @@ class AemProxyEndpointTest {
 //		}
 	}
 
+	@Test
+	void proxyTest() throws Exception {
+	    try (final WebClient webClient = new WebClient()) {
+	        String baseUri = getBaseUriString(port) + "/FluentForms/Html5FormsServiceRenderHtml5Form" + "?form=" + SAMPLE_FILES_DIR.resolve("SampleForm.xdp").toAbsolutePath();
+			final HtmlPage page = webClient.getPage(baseUri);
+	        assertEquals("LC Forms", page.getTitleText());
+	    }
+	    List.of(
+	    		/* "/content/xfaforms/profiles/default.html", */ // This fails for some reason however it's not essential to the test because if this were truly not working, none of the other calls would be made.
+	    		/* "/libs/granite/csrf/token.json", */ // This is not tested because it doesn't always happen (depending on the timings).
+	    		"/etc.clientlibs/fd/xfaforms/clientlibs/I18N/en.js",
+	    		"/etc.clientlibs/fd/xfaforms/clientlibs/profile.css",
+	    		"/etc.clientlibs/fd/xfaforms/clientlibs/profile.js",
+	    		"/etc.clientlibs/clientlibs/granite/jquery/granite/csrf.js"
+	    		/* "/etc.clientlibs/toggles.json", */ // This fails because of FluentForm issue #52, uncomment this when that is fixed.
+	    		)
+	    	.forEach(url->verify(getRequestedFor(urlPathEqualTo(url))));
+	}
+	
+	// In order to re-record the AEM interactions for Wiremock emulation, you need to:
+	//  1) run a local AEM server
+	//  2) set the WIREMOCK_RECORDING variable to true
+	//  3) then run this test.
+	// 
+	// It will record the interactions with the AEM server.
+	// Don't forget to set the WIREMOCK_RECORDING variable back to false after you are done.
+	//
+	// Note: THe recordings may require modification.  As of AEM 6.5 LTS, the required changes are:
+	//  * Two calls to /content/xfaforms/profiles/default.html - One returns a 401, the other returns the form.
+	//    This is because this HTMLUnit emulates a browser.  The 401 recording can be deleted since the FluentForms code
+	//    sends a pre-emptive authentication header.
+	//  * the /etc.clientlibs/fd/xfaforms/clientlibs/I18N/en_US recording must be modified to make the _US optional.
+	//    It appears that the FluentForms call does not include the _US suffix.
+	@Disabled("This test is not really a test but it is used to record interactions with the AEM server.")
+	@Test
+	void aemTest() throws Exception {
+	    DefaultCredentialsProvider userCredentials = new DefaultCredentialsProvider();
+	    userCredentials.addCredentials("admin", "admin".toCharArray());
+	    try (final WebClient webClient = new WebClient()) {
+	    	webClient.setCredentialsProvider(userCredentials);
+	        String baseUri = "http://localhost:" + WIREMOCK_PORT + "/content/xfaforms/profiles/default.html?contentRoot=crx:///content/dam/formsanddocuments/sample-forms&template=SampleForm.xdp";
+			final HtmlPage page = webClient.getPage(baseUri);
+	        assertEquals("LC Forms", page.getTitleText());
+
+//	        final String pageAsXml = page.asXml();
+//	        assertTrue(pageAsXml.contains("<body class=\"topBarDisabled\">"), "Does not contain topBarDisabled");
+//
+//	        final String pageAsText = page.asNormalizedText();
+//	        assertTrue(pageAsText.contains("Support for the HTTP and HTTPS protocols"));
+	    }
+	    List.of("/content/xfaforms/profiles/default.html", 
+	    		"/etc.clientlibs/toggles.json", 
+	    		"/libs/granite/csrf/token.json",
+	    		"/etc.clientlibs/fd/xfaforms/clientlibs/I18N/en_US.js",
+	    		"/etc.clientlibs/fd/xfaforms/clientlibs/profile.css",
+	    		"/etc.clientlibs/fd/xfaforms/clientlibs/profile.js"
+	    		)
+	    	.forEach(url->verify(getRequestedFor(urlPathEqualTo(url))));
+
+	}
+		
 	private static String getBaseUriString(int port) {
 		return getBaseUri(port).toString();
 	}
